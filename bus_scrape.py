c9457cc4bf4250fcc14eb3cc05303b1dd324250d
npicciano79 <npicciano79@gmail.com>
2021-10-13 17:56:35 -0400

    bus_scrape.py

--- bus_scrape.py
+++ bus_scrape.py
@@ -1,2 +1,21 @@
-print("hi fucker")
-#test test test test test 
+#scrape business listing from new haven commerce website
+#https://www.gnhcc.com/list
+
+from bs4 import BeautifulSoup
+import csv
+import requests
+
+
+url="https://www.gnhcc.com/list"
+
+def scrape(url):
+    req=requests.get(url)
+    print(req)
+
+
+
+
+
+
+if __name__=="__main__":
+    scrape(url)
