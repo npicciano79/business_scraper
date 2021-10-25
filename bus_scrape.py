92375dd171f8dcf365c172d20c53cd4011554bdd
npicciano79 <npicciano79@gmail.com>
2021-10-25 11:18:40 -0400

    bus_scrape.py

--- bus_scrape.py
+++ bus_scrape.py
@@ -10,8 +10,14 @@ url="https://www.gnhcc.com/list"
 
 def scrape(url):
     req=requests.get(url)
-    soup=BeautifulSoup(req.content, 'html5')
-    print(soup)
+    soup=BeautifulSoup(req.content, 'html5lib')
+    
+    #find business categories
+
+    cat=soup.find('div',class_="row gz-cards gz-directory-cards gz-no-cards")
+    print(f"cat--- {cat} ---cat")
+    #test=soup.find("div",class_="row gz-directory-cards gz-no-cards")
+    #print(f"test: {test}")
 
 
 
